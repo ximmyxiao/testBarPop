593624c5c072dc0cd70b6ee52b8d9802517e4bc4
Piao Piao <94313353@qq.com>
2017-04-10 18:05:01 +0800

    first commit

---
+++ testBarPop/SubViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubViewController.h
+//  testBarPop
+//
+//  Created by Piao Piao on 2017/4/10.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubViewController : UIViewController
+
+@end
--- testBarPop/ViewController.m
+++ testBarPop/ViewController.m
@@ -18,12 +18,33 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (void)viewWillAppear:(BOOL)animated
+{
+    NSLog(@"red viewWillAppear");
+    
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar.layer removeAllAnimations];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    self.navigationController.interactivePopGestureRecognizer.delegate = self;
+    
+}
 
+- (void)viewWillDisappear:(BOOL)animated
+{
+    NSLog(@"red viewWillDisappear");
+    
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
 
 @end
